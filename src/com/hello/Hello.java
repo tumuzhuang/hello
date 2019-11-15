5c8f7059c8e6f82b532bed6ec2dcfc6b8937e33f
lolor <1721753977@qq.com>
2019-11-15 16:20:05 +0800

    上传初始项目hello

---
+++ src/com/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.hello;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("hellochina");
+        System.out.println("helloxian");
+    }
+
+
+}
